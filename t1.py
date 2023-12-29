eaaa547c6ad12297bd9bd030b689c17de522b6a9
mdgolammortuja <155061680+mdgolammortuja@users.noreply.github.com>
2023-12-29 09:36:41 +0000

    commit

--- t1.py
+++ t1.py
@@ -1,6 +1,6 @@
 # Task: Read the contents of 'data.txt' and print each line with line numbers
-
 with open('data.txt', 'r') as file:
     # Use Copilot to complete the code to read and print each line with line numbers
-    for line in file:
+    for line_number, line in enumerate(file, start=1):
         # TODO: Complete the code here
+        print(f"Line {line_number}: {line.strip()}")
