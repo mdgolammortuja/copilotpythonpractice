5a83aa2e39256f20cc0bda1938dae7b47612b385
mdgolammortuja <155061680+mdgolammortuja@users.noreply.github.com>
2023-12-29 09:39:06 +0000

    ffgj

--- t2.py
+++ t2.py
@@ -1,7 +1,11 @@
-# Task: Create a function that reverses the words in a given sentence
-
 def reverse_words(sentence):
     words = sentence.split()
     # Use Copilot to complete the code to reverse the words in the sentence
     reversed_sentence = ' '.join(reversed(words))
     # TODO: Return the reversed sentence
+    return reversed_sentence
+
+# Example usage:
+original_sentence = "Hello World"
+result = reverse_words(original_sentence)
+print(result)
